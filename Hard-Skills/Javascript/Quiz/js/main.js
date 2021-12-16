e91a4b6cc14c902dae75c3d5f1da70dbbf0b7d5a
Mikevanschaik <93578567+Mikevanschaik@users.noreply.github.com>
2021-12-16 14:55:55 +0100

    updates javacript

---
+++ Hard-Skills/Javascript/Quiz/js/main.js
@@ -0,0 +1,12 @@
+///// construct divs //////
+
+const quizDiv = document.createElement('div')
+quizDiv.classList.add('quizDiv')
+container.appendChild(quizDiv)
+
+const answerDiv = document.createElement('div')
+answerDiv.classList.add('answerDiv')
+quizDiv.appendChild(answerDiv)
+
+
+
